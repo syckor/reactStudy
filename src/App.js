8bc098ffd95f2e30fda6f4c45f1d9ca821d01d96
syckor <syckor@naver.com>
2023-12-11 22:50:15 +0900

    react state basic

--- src/App.js
+++ src/App.js
@@ -2,34 +2,15 @@
 //import './App.css';
 import MyHeader from './MyHeader';
 import MyFooter from './MyFooter'
+import Counter from './Counter';
 
 function App() {
-  let name = "react";
 
-  const style = {
-    App: {
-      backgroundColor: "black",
-    },
-    h2: {
-      color: "red",
-    },
-    bold_text :{
-      color: "green",
-    },
-  };
-  const func = () => {
-    return 'func'
-  }
-
-  const number = 5;
 
   return (
-    <div style={style.App}>
+    <div>
       <MyHeader/>
-        <h2 style={style.h2}>Hello {name} world {1+2} {func}</h2>
-        <b style={style.bold_text} id="bold_text">
-          {number}는 : {number % 2 === 0 ? "even" : "odd"}
-        </b>
+        <Counter />
       <MyFooter/> 
     </div>
   );
